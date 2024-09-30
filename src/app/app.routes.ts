57f18779ab1b361e04bbe3e74fb062eac27196c8
Moro Díaz <aroa.moro-diaz@capgemini.com>
2024-09-30 16:27:46 +0200

    Añadido ruta para el home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [ {
+  path: 'home',
+  loadComponent: () => import('../components/homePage/homePage.component').then(m => m.HomePageComponent),
+},
+{
+  path: '',
+  redirectTo: '/home',
+  pathMatch: 'full'
+}];
