67bc6da1034d7234fb2f01789132814a74524c94
Moro Díaz <aroa.moro-diaz@capgemini.com>
2024-09-30 18:00:45 +0200

    Añadido angular material a falta de arreglar scroll vertical

--- src/components/listaUsuarios/listaUsuarios.component.ts
+++ src/components/listaUsuarios/listaUsuarios.component.ts
@@ -1,14 +1,68 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-lista-usuarios',
   standalone: true,
   imports: [
     CommonModule,
+    MatCardModule
   ],
   templateUrl: './listaUsuarios.component.html',
   styleUrl: './listaUsuarios.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ListaUsuariosComponent { }
+export class ListaUsuariosComponent {
+  usuarios = [
+    {
+      nombre: 'Aroa',
+      apellido: 'Moro',
+      telefono: '089898'
+    },
+    {
+      nombre: 'Aroa',
+      apellido: 'Moro',
+      telefono: '089898'
+    },
+    {
+      nombre: 'Aroa',
+      apellido: 'Moro',
+      telefono: '089898'
+    },{
+      nombre: 'Aroa',
+      apellido: 'Moro',
+      telefono: '089898'
+    },
+    {
+      nombre: 'Aroa',
+      apellido: 'Moro',
+      telefono: '089898'
+    },
+    {
+      nombre: 'Aroa',
+      apellido: 'Moro',
+      telefono: '089898'
+    },
+    {
+      nombre: 'Aroa',
+      apellido: 'Moro',
+      telefono: '089898'
+    },
+    {
+      nombre: 'Aroa',
+      apellido: 'Moro',
+      telefono: '089898'
+    },
+    {
+      nombre: 'Aroa',
+      apellido: 'Moro',
+      telefono: '089898'
+    },
+    {
+      nombre: 'Aroa',
+      apellido: 'Moro',
+      telefono: '089898'
+    }
+  ]
+}
