89b04872f47e58ef9ff1c8574be61424a2f957c0
Moro Díaz <aroa.moro-diaz@capgemini.com>
2024-10-01 13:53:16 +0200

    Añadida validacion de que sean más de 4 caracteres

--- src/shared/cabecera/cabecera.component.ts
+++ src/shared/cabecera/cabecera.component.ts
@@ -1,11 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cabecera',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './cabecera.component.html',
   styleUrl: './cabecera.component.css',
 })
-export class CabeceraComponent { }
+export class CabeceraComponent {
+
+  form: FormGroup;
+
+  constructor (private fb: FormBuilder){
+    this.form = this.fb.group({
+      busqueda: ['', [Validators.required, Validators.minLength(4)]]
+    })
+  }
+
+}
