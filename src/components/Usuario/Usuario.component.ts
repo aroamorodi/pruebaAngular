a57a42cc1191b1a5186df1b940921fe34defa0ff
CORP\amorodia <aroa.moro-diaz@capgemini.com>
2024-10-14 09:31:38 +0200

    Quitar console.log

--- src/components/Usuario/Usuario.component.ts
+++ src/components/Usuario/Usuario.component.ts
@@ -29,7 +29,6 @@ export class UsuarioComponent implements OnInit {
 
   ngOnInit(): void {
     setTimeout(() => {
-      console.log(this.activateRoute.params)
       this.activateRoute.params
         .pipe(
           switchMap(({ id }) => this.gitService.searchUserById(id)) //tomar los params
